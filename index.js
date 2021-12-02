1ab7e6e10db333d12eb2a5699a4720bf5639c3ed
mira-kine <mirak0419@gmail.com>
2021-12-02 12:56:03 -0800

    pushing after partner practice on printUserInfo

--- index.js
+++ index.js
@@ -3,26 +3,48 @@
 // OUTPUT: the value of the 'name' attribute i.e. Benny
 // REQS: use destructuring and the function should be a single line
 
-export const getName = () => {};
+export const getName = ({ name }) => name;
 
 // INPUT: an object with a nested "address" attribute such as
 //   {name: 'Bob Smith', address: {street: 'Main Street', number: 123, city: 'Anytown', country: 'USA}}
 // OUTPUT: the string 'Bob Smith lives at 123 Main Street, Anytown, USA'
 // REQS: use destructuring and template literals
 
-export const printAddress = () => {};
+export const printAddress = ({ name, address: { street, number, city, country } }) => {
+  return `${name} lives at ${number} ${street}, ${city}, ${country}`;
+};
 
 // REFACTOR CHALLENGE
 // Refactor this function so that all values in the object are destructured
 // as part of the funciton definitions (i.e. there should be no dots in the template literals)
-export const printUserInfo = () => {};
-
+export const printUserInfo = ({
+  username,
+  name: { first, last },
+  info: {
+    favorites: { color, food },
+    pet: { name },
+    address: { number, street, city, country },
+  },
+}) => {
+  return `
+    Username: ${username},
+    Full Name: ${first} ${last},
+    Favorite Color: ${color},
+    Favorite Food: ${food},
+    Pet Name: ${name},
+    Address: ${number} ${street}, ${city}, ${country}
+    `;
+};
 // INPUT: an unknown number of input arguments, all numbers
 // OUTPUT: their sum
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {};
+export const getSum = (...rest) => {
+  let sum = 0;
+  rest.forEach((num) => (sum += num));
+  return sum;
+};
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
